a4eae1444a5454263a5d4f3e8020924fa8efc1c5
yinqian1 <yin.qian1@northeastern.edu>
2026-02-28 06:10:08 -0800

    GTM fix

--- js/common.js
+++ js/common.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-    initGTM();
-
     $.get("common", function(data) {
         console.log("Content loaded successfully:", data);
         $(".Nav").replaceWith($(data).filter(".fixed-navbar"));
@@ -16,21 +14,4 @@ function arrayContainsArray(superset, subset) {
     return subset.every(function(value) {
         return (superset.indexOf(value) >= 0);
     });
-}
-
-function initGTM() {
-  const GTM_ID = "GTM-NMDC5OHX";
-
-  if (window.dataLayer) return;
-
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js'
-  });
-
-  const script = document.createElement("script");
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`;
-  document.head.appendChild(script);
 }
